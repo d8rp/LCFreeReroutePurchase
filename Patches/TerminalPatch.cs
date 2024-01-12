3edcb2e3f60c353018123fc225b760565ea14e74
d8rp <thanh062007gmail.com>
2024-01-12 21:01:59 +0700

    Adding existing files

---
+++ Patches/TerminalPatch.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCFreeReroutePurchase.Patches
+{
+    [HarmonyPatch(typeof(Terminal))]
+    internal class TerminalPatch
+    {
+        [HarmonyPatch("LoadNewNodeIfAffordable")]
+        [HarmonyPrefix]
+        static void OnLoadNewNodeIfAffordable(ref Terminal __instance, TerminalNode node)
+        {
+            if (node.buyRerouteToMoon != -1)
+            {
+                node.itemCost = 0;
+            }
+        }
+        [HarmonyPatch("LoadNewNode")]
+        [HarmonyPrefix]
+        static void OnLoadNewNode(ref Terminal __instance, TerminalNode node, ref int ___totalCostOfItems)
+        {
+            if (node.buyRerouteToMoon != -1)
+            {
+                ___totalCostOfItems = 0;
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using LCFreeReroutePurchase.Patches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCFreeReroutePurchase
+{
+
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class TutorialModBase : BaseUnityPlugin
+    {
+        private const string modGUID = "Derp.TutorialMod";
+        private const string modName = "Tutorial Mod";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static TutorialModBase Instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("Free reroute purchase has awaken :D");
+
+            harmony.PatchAll(typeof(TutorialModBase));
+            harmony.PatchAll(typeof(TerminalPatch));
+        }
+    }
+}
